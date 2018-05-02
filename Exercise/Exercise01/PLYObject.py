bcd1830f6e297cf28b563df1a871cfeb8652e922
Stefan <Stefan_Hahn1@gmx.net>
2018-05-02 09:14:45 +0200

    Implemented test code

--- Exercise/Exercise01/PLYObject.py
+++ Exercise/Exercise01/PLYObject.py
@@ -109,12 +109,42 @@ class PLYObject:
 
 # Leitet eine Testumgebung ein. Der folgende Code wird bei der Korrektur nicht ausgefuehrt(!) und kann daher beliebig abgeaendert werden.
 if __name__ == "__main__":
-	o = PLYObject("lucy.ply")
-	o.flipX()
-	o.flipFaces()  # sinnvoll in Kombination mit Spiegelungen, damit danach nicht die Rueckseiten von Flaechen zu sehen sind
-	# alternative Tests:
-	# o.translate(numpy.array([2000,500,-1000]))
-	# o.scale(0.4)
-	# o.rotateX(1.1)
-	# ...
-	o.write("lucy_modified.ply")
+	import copy
+
+	print('Loading lucy.ply')
+
+	o1 = PLYObject('lucy.ply')
+	o2 = copy.deepcopy(o1)
+	o3 = copy.deepcopy(o1)
+	o4 = copy.deepcopy(o1)
+
+	print('Loaded lucy.ply')
+
+	# test translate
+	print('Translating')
+	o1.translate([100, 200, 300])
+	print('Translated')
+	o1.write('lucy_translate.ply')
+	print('Wrote lucy_translate.ply')
+
+	# test scale
+	print('Scaling')
+	o2.scale(0.5)
+	print('Scaled')
+	o2.write('lucy_scale.ply')
+	print('Wrote lucy_scale.ply')
+
+	# test rotateX
+	print('Rotating')
+	o3.rotateX(0.5 * numpy.pi)
+	print('Rotated')
+	o3.write('lucy_rotate.ply')
+	print('Wrote lucy_rotate.ply')
+
+	# test flipX
+	print('Flipping')
+	o4.flipX()
+	o4.flipFaces()
+	print('Flipped')
+	o4.write('lucy_flip.ply')
+	print('Wrote lucy_flip.ply')
