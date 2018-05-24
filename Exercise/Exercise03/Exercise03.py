326d2fc642fda999564f176bda68040fbe724bf2
Stefan <Stefan_Hahn1@gmx.net>
2018-05-24 13:41:54 +0200

    Added better logging as well as performance measurement

--- Exercise/Exercise03/Exercise03.py
+++ Exercise/Exercise03/Exercise03.py
@@ -1,23 +1,45 @@
+import time
+
 from PLYObject import PLYObject
 
+print('Loading sphere')
 p = PLYObject('sphere.ply')
-print(p.getVertices().shape)
+print('Number of vertices: {:d}'.format(p.getVertices().shape[1]))
 
+print('Fitting sphere')
+t0 = time.perf_counter()
 sphereInfo = p.fitSphere()
-print(sphereInfo)
+t1 = time.perf_counter()
+print('Took {:.4f} s'.format(t1 - t0))
+print('Fitted sphere: {!s}'.format(sphereInfo))
 
+print('Generating sphere vertices')
+t0 = time.perf_counter()
 sphere = PLYObject.from_sphere(sphereInfo)
-print(sphere.getVertices().shape)
+t1 = time.perf_counter()
+print('Took {:.4f} s'.format(t1 - t0))
+print('Number of vertices: {:d}'.format(sphere.getVertices().shape[1]))
+print('Writing generated sphere vertices')
 sphere.write('sphere_generated.ply')
 
 print('')
 
+print('Loading plane')
 p = PLYObject('plane.ply')
-print(p.getVertices().shape)
+print('Number of vertices: {:d}'.format(p.getVertices().shape[1]))
 
+print('Fitting plane')
+t0 = time.perf_counter()
 planeInfo = p.fitPlane()
-print(planeInfo)
+t1 = time.perf_counter()
+print('Took {:.4f} s'.format(t1 - t0))
+print('Fitted plane: {!s}'.format(planeInfo))
 
+print('Generating plane vertices')
+t0 = time.perf_counter()
 plane = PLYObject.from_plane(planeInfo)
-print(plane.getVertices().shape)
+t1 = time.perf_counter()
+print('Took {:.4f} s'.format(t1 - t0))
+print('Number of vertices: {:d}'.format(plane.getVertices().shape[1]))
+print('Writing generated plane vertices')
 plane.write('plane_generated.ply')
