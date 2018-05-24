2dc108df278c163b6a9865b1d94e4788bec97fea
Stefan <Stefan_Hahn1@gmx.net>
2018-05-24 17:07:10 +0200

    Also handle sphere2 data in test script

--- Exercise/Exercise03/Exercise03.py
+++ Exercise/Exercise03/Exercise03.py
@@ -24,6 +24,28 @@ sphere.write('sphere_generated.ply')
 
 print('')
 
+print('Loading sphere2')
+p = PLYObject('sphere2.ply')
+print('Number of vertices: {:d}'.format(p.getVertices().shape[1]))
+
+print('Fitting sphere')
+t0 = time.perf_counter()
+sphereInfo = p.fitSphere()
+t1 = time.perf_counter()
+print('Took {:.4f} s'.format(t1 - t0))
+print('Fitted sphere2: {!s}'.format(sphereInfo))
+
+print('Generating sphere2 vertices')
+t0 = time.perf_counter()
+sphere = PLYObject.from_sphere(sphereInfo)
+t1 = time.perf_counter()
+print('Took {:.4f} s'.format(t1 - t0))
+print('Number of vertices: {:d}'.format(sphere.getVertices().shape[1]))
+print('Writing generated sphere2 vertices')
+sphere.write('sphere2_generated.ply')
+
+print('')
+
 print('Loading plane')
 p = PLYObject('plane.ply')
 print('Number of vertices: {:d}'.format(p.getVertices().shape[1]))
