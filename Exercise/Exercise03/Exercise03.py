a6126bdb3a21f691288b37da86cf171235593f79
Stefan <Stefan_Hahn1@gmx.net>
2018-05-24 12:33:28 +0200

    Removed unnecessary check for namespace

--- Exercise/Exercise03/Exercise03.py
+++ Exercise/Exercise03/Exercise03.py
@@ -1,24 +1,23 @@
 from PLYObject import PLYObject
 
-if __name__=="__main__":
-	p = PLYObject('sphere.ply')
-	print(p.getVertices().shape)
+p = PLYObject('sphere.ply')
+print(p.getVertices().shape)
 
-	sphereInfo = p.fitSphere()
-	print(sphereInfo)
+sphereInfo = p.fitSphere()
+print(sphereInfo)
 
-	sphere = PLYObject.from_sphere(sphereInfo)
-	print(sphere.getVertices().shape)
-	sphere.write('sphere_generated.ply')
+sphere = PLYObject.from_sphere(sphereInfo)
+print(sphere.getVertices().shape)
+sphere.write('sphere_generated.ply')
 
-	print('')
+print('')
 
-	p = PLYObject('plane.ply')
-	print(p.getVertices().shape)
+p = PLYObject('plane.ply')
+print(p.getVertices().shape)
 
-	planeInfo = p.fitPlane()
-	print(planeInfo)
+planeInfo = p.fitPlane()
+print(planeInfo)
 
-	plane = PLYObject.from_plane(planeInfo)
-	print(plane.getVertices().shape)
-	plane.write('plane_generated.ply')
+plane = PLYObject.from_plane(planeInfo)
+print(plane.getVertices().shape)
+plane.write('plane_generated.ply')
