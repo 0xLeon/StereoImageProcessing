74be0a6b89ae6fc59697d7642f66e3426f1efeb6
Stefan <Stefan_Hahn1@gmx.net>
2018-06-01 18:15:56 +0200

    Added RealSense script for automatic measurement series generation

---
+++ Lab/GetDepthFrames.py
@@ -0,0 +1,138 @@
+import argparse
+import msvcrt
+import os
+import pickle
+import sys
+import time
+
+import attr
+import cv2
+import numpy as np
+import pyrealsense2 as rs
+
+@attr.s
+class RSIntrinsics(object):
+	ppx = attr.ib(type=float)
+
+
+def intrinsics_to_dict(intrinsics: rs.intrinsics):
+	return {
+		'ppx': intrinsics.ppx,
+		'ppy': intrinsics.ppy,
+		'fx': intrinsics.fx,
+		'fy': intrinsics.fy,
+		'width': intrinsics.width,
+		'height': intrinsics.height,
+		'model': int(intrinsics.model),
+		'coeffs': intrinsics.coeffs,
+	}
+
+def dict_to_intrinsics(d: dict):
+	intrinsics = rs.intrinsics()
+	intrinsics.ppx = d['ppx']
+	intrinsics.ppy = d['ppy']
+	intrinsics.fx = d['fx']
+	intrinsics.fy = d['fy']
+	intrinsics.width = d['width']
+	intrinsics.height = d['height']
+	intrinsics.model = rs.distortion(d['model'])
+	intrinsics.coeffs = d['coeffs']
+
+	return intrinsics
+
+def getDepthFrames(nImages=10, resolution=(1280,720), fps=30, sleepTime=5, laser=False, outputFolder='./'):
+	if nImages < 1:
+		raise ValueError()
+
+	if sleepTime < 0:
+		raise ValueError()
+
+	if not os.path.isdir(outputFolder):
+		os.makedirs(outputFolder)
+
+	pointcloud = rs.pointcloud() # type: rs.pointcloud
+	points = rs.points() # typr: rs.points
+
+	config = rs.config() # type: rs.config
+	config.enable_stream(rs.stream.depth, resolution[0], resolution[1], rs.format.z16, fps)
+	config.enable_stream(rs.stream.color, resolution[0], resolution[1], rs.format.rgb8, fps)
+
+	align = rs.align(rs.stream.depth) # type: rs.align
+
+	pipeline = rs.pipeline() # type: rs.pipeline
+	profile = pipeline.start(config) # type: rs.pipeline_profile
+	device = profile.get_device() # type: rs.device
+	depthSensor = device.first_depth_sensor() # type: rs.depth_sensor
+
+	if depthSensor.supports(rs.option.emitter_enabled):
+		depthSensor.set_option(rs.option.emitter_enabled, 1.0 if laser else 0.0)
+	elif laser:
+		raise EnvironmentError('Device does not support laser')
+
+	np.savez(
+		os.path.join(outputFolder, 'depthscale.npz'),
+		depthscale=depthSensor.get_depth_scale(),
+	)
+
+	counter = 0
+
+	while True:
+		print('Waiting for frame {:d}'.format(counter))
+		frames = pipeline.wait_for_frames() # type: rs.frameset
+		alignedFrames = align.process(frames) # type: rs.frameset
+		depth = alignedFrames.get_depth_frame() # type: rs.depth_frame
+		color = alignedFrames.get_color_frame() # type: rs.video_frame
+
+		if not depth:
+			print('No depth data, retry...')
+			continue
+
+		if not color:
+			print('No color data, retry...')
+			continue
+
+		print('Depth data available')
+		depthImage = np.asanyarray(depth.get_data())
+		depthIntrinsics = intrinsics_to_dict(depth.get_profile().as_video_stream_profile().get_intrinsics())
+
+		print('Writing raw numpy file')
+		np.savez(os.path.join(outputFolder, '{:d}.depth.image.npz'.format(counter)), depthImage=depthImage)
+		with open(os.path.join(outputFolder, '{:d}.depth.intrinsics.pkl'.format(counter)), 'wb') as f:
+			pickle.dump(depthIntrinsics, f)
+
+		print('Generating point cloud')
+		pointcloud.map_to(color)
+		points = pointcloud.calculate(depth) # type: rs.points
+
+		print('Writing point cloud to PLY file')
+		points.export_to_ply(os.path.join(outputFolder, '{:d}.pointcloud.ply'.format(counter)), color)
+
+		counter = counter + 1
+
+		if counter >= nImages:
+			break
+
+		print('Sleeping before next frame')
+		print('')
+		time.sleep(sleepTime)
+
+	pipeline.stop()
+
+def main(args=None):
+	if args is None:
+		args = sys.argv[1:]
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--nimages', type=int, default=10, help='Number of depth frames to capture')
+	parser.add_argument('--resolution', default='1280,720', help='Sensor resolution, defaults to to 1280,720')
+	parser.add_argument('--fps', type=int, default=30, help='Sensor framerate, defaults to to 30')
+	parser.add_argument('--sleeptime', type=int, default=5, help='Time to sleep between capturing depth frames [s]')
+	parser.add_argument('--laser', default=False, action='store_true', help='Activate laser, if supported')
+	parser.add_argument('--output', default='./', help='Output path, defaults to current working dir')
+	args = parser.parse_args(args)
+
+	resolution = (*map(int, args.resolution.split(',')),)
+	getDepthFrames(args.nimages, resolution, args.fps, args.sleeptime, args.laser, args.output)
+
+if __name__ == '__main__':
+	main()
---
+++ Lab/GetRealsenseProfiles.py
@@ -0,0 +1,41 @@
+import pyrealsense2 as rs
+
+def main():
+	for device in rs.context().query_devices():
+		name = device.get_info(rs.camera_info.name).replace(' ', '_')
+
+		for sensor in device.query_sensors(): # type: rs.sensor
+			filename = '{:s}__{:s}.txt'
+
+			if sensor.is_depth_sensor():
+				filename = filename.format(name, 'Depth')
+				sensor = sensor.as_depth_sensor()
+			# elif sensor.is_roi_sensor():
+			# 	filename = filename.format(name, 'ROI')
+			# 	sensor.as_roi_sensor()
+			else:
+				filename = filename.format(name, 'Color')
+
+			with open(filename, 'w') as f:
+				for profile in sensor.get_stream_profiles(): # type: rs.stream_profile
+					if profile.is_video_stream_profile():
+						profile = profile.as_video_stream_profile() # type: rs.video_stream_profile
+
+						print('{:s}({:d}) {:d}x{:d} @ {:d}fps {:s}'.format(
+							str(profile.stream_type()).split('.')[-1].title(),
+							profile.stream_index(),
+							profile.width(),
+							profile.height(),
+							profile.fps(),
+							str(profile.format()).split('.')[-1].upper(),
+						), file=f)
+					else:
+						print('{:s}({:d}) @ {:d}fps {:s}'.format(
+							str(profile.stream_type()).split('.')[-1].title(),
+							profile.stream_index(),
+							profile.fps(),
+							str(profile.format()).split('.')[-1].upper(),
+						), file=f)
+
+if __name__ == '__main__':
+	main()
