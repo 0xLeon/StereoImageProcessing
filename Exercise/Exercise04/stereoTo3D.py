a0d168128b8fe2d7386acb5f11add5aaef20f593
Stefan <Stefan_Hahn1@gmx.net>
2018-06-01 23:51:56 +0200

    Implement FLANN feature matching
    Display matched images for debug info.

--- Exercise/Exercise04/stereoTo3D.py
+++ Exercise/Exercise04/stereoTo3D.py
@@ -36,7 +36,37 @@ def drawMatchedImages(imgA, kpA, imgB, kpB, matches, matchesMask):
 	plt.show()
 
 def main():
-	pass
+	camA = readCameraData('camA')
+	camB = readCameraData('camB')
+
+	imgA = cv.imread('camA_image.jpg')
+	imgB = cv.imread('camb_image.jpg')
+
+	orb = cv.ORB_create(50000)
+	flann = cv.FlannBasedMatcher(
+		dict(algorithm=6, table_number=12, key_size=20, multi_proble_level=2),
+		dict(checks=50),
+	)
+
+	kpA, desA = orb.detectAndCompute(imgA, None)
+	kpB, desB = orb.detectAndCompute(imgB, None)
+
+	matches = flann.knnMatch(desA, desB, k=2)
+	matchesMask = [[0, 0] for i in range(len(matches))]
+
+	c = 0
+
+	for i, (m, n) in enumerate(matches):
+		if m.distance < 0.8 * n.distance:
+			matchesMask[i] = [1, 0]
+
+			c += 1
+
+	print(c)
+
+	drawMatchedImages(imgA, kpA, imgB, kpB, matches, matchesMask)
+
+	return
 
 if __name__ == '__main__':
 	main()
