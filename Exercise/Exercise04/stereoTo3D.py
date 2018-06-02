e4987cc06eb0caccbca491da37ecc19e418d486e
Stefan <Stefan_Hahn1@gmx.net>
2018-06-02 16:29:31 +0200

    Moved matching to separate function

--- Exercise/Exercise04/stereoTo3D.py
+++ Exercise/Exercise04/stereoTo3D.py
@@ -79,6 +79,25 @@ def saveMatches(matches, matchesMask, file):
 	else:
 		pickle.dump(dict(matches=matches, matchesMask=matchesMask), file)
 
+def matchImages(imgA, imgB, nFeatures=50000, qualityThreshold=0.8):
+	orb = cv.ORB_create(nFeatures)
+	flann = cv.FlannBasedMatcher(
+		dict(algorithm=6, table_number=12, key_size=20, multi_proble_level=2),
+		dict(checks=50),
+	)
+
+	kpA, desA = orb.detectAndCompute(imgA, None)
+	kpB, desB = orb.detectAndCompute(imgB, None)
+
+	matches = flann.knnMatch(desA, desB, k=2)
+	matchesMask = [[0, 0] for i in range(len(matches))]
+
+	for i, (m, n) in enumerate(matches):
+		if m.distance < qualityThreshold * n.distance:
+			matchesMask[i] = [1, 0]
+
+	return kpA, desA, kpB, desB, matches, matchesMask
+
 def generateDisparityMap():
 	pass
 
@@ -105,22 +124,7 @@ def main():
 	imgA = cv.imread('camA_image.jpg')
 	imgB = cv.imread('camb_image.jpg')
 
-	orb = cv.ORB_create(50000)
-	flann = cv.FlannBasedMatcher(
-		dict(algorithm=6, table_number=12, key_size=20, multi_proble_level=2),
-		dict(checks=50),
-	)
-
-	kpA, desA = orb.detectAndCompute(imgA, None)
-	kpB, desB = orb.detectAndCompute(imgB, None)
-
-	matches = flann.knnMatch(desA, desB, k=2)
-	matchesMask = [[0, 0] for i in range(len(matches))]
-
-	for i, (m, n) in enumerate(matches):
-		if m.distance < 0.8 * n.distance:
-			matchesMask[i] = [1, 0]
-
+	kpA, desA, kpB, desB, matches, matchesMask = matchImages(imgA, imgB)
 
 	drawMatchedImages(imgA, kpA, imgB, kpB, matches, matchesMask)
 
