ce9e37d122c200f4ca2d2ae61743e8f30d15d581
Stefan <Stefan_Hahn1@gmx.net>
2018-06-06 00:51:24 +0200

    Use more informative output folder names

--- Lab/depth-5.0m-1280x720-6-NoLaser.bat
+++ Lab/depth-5.0m-1280x720-6-NoLaser.bat
@@ -1,2 +1,2 @@
 @echo off
-py GetDepthFrames.py --nimages=5 --resolution=1280,720 --fps=6 --output=data/depth-5.0m-1280x720-6-NoLaser/
+py GetDepthFrames.py --nimages=5 --resolution=1280,720 --fps=6 --output=data/RealSense-D415-Plane-5.0m-1280x720-6-NoLaser/
--- Lab/depth-5.0m-640x480-6-Laser.bat
+++ Lab/depth-5.0m-640x480-6-Laser.bat
@@ -1,2 +1,2 @@
 @echo off
-py GetDepthFrames.py --nimages=5 --resolution=640,480 --fps=6 --laser --output=data/depth-5.0m-640x480-6-Laser/
+py GetDepthFrames.py --nimages=5 --resolution=640,480 --fps=6 --laser --output=data/RealSense-D415-Plane-5.0m-640x480-6-Laser/
