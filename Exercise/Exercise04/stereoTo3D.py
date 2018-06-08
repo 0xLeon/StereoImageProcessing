6009b42885eae5b7bff41d0c6b12e6bea332e516
Stefan <Stefan_Hahn1@gmx.net>
2018-06-08 19:06:11 +0200

    Fiddled around with direct calculation again
    Still wrong…

--- Exercise/Exercise04/stereoTo3D.py
+++ Exercise/Exercise04/stereoTo3D.py
@@ -210,8 +210,8 @@ def main(camNameA='camA', camNameB='camB', readMatch='', output='./'):
 	# not sure about calculation of direction vectors from undistorted points
 	# the third coordinate for each undistorted u,v point is set to 1 in the
 	# previous function call, which I'm not sure about.
-	directionsA = (ukpA - camA.translation).dot(np.linalg.inv(camA.rotation).T) - cA
-	directionsB = (ukpB - camB.translation).dot(np.linalg.inv(camB.rotation).T) - cA
+	directionsA = (ukpA - camA.translation).dot(np.linalg.inv(camA.rotation).T)
+	directionsB = (ukpB - camB.translation).dot(np.linalg.inv(camB.rotation).T)
 
 	# vector from second to first projection center
 	directionCACB = cB - cA
