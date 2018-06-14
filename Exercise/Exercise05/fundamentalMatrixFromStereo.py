c1ee9b4dbe1dd0345294781e5249933c391f2fc5
Stefan Hahn <Stefan_Hahn1@gmx.net>
2018-06-14 21:15:12 +0200

    Added basic Python code for exercise 05

---
+++ Exercise/Exercise05/fundamentalMatrixFromStereo.py
@@ -0,0 +1,16 @@
+import argparse
+
+import cv2
+import numpy as np
+
+def main():
+	pass
+
+def main_cli(args=None):
+	parser = argparse.ArgumentParser()
+	args = parser.parse_args(args)
+
+	main()
+
+if __name__ == '__main__':
+	main_cli()
