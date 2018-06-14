866d4bb23b1bfcb4a6b0a7e5906f43ccf626c9c0
Stefan Hahn <Stefan_Hahn1@gmx.net>
2018-06-14 22:08:28 +0200

    Added command line argument for input images

--- Exercise/Exercise05/fundamentalMatrixFromStereo.py
+++ Exercise/Exercise05/fundamentalMatrixFromStereo.py
@@ -3,14 +3,18 @@ import argparse
 import cv2
 import numpy as np
 
-def main():
+def main(images):
 	pass
 
 def main_cli(args=None):
 	parser = argparse.ArgumentParser()
+	parser.add_argument('images', default=['a.jpg', 'b.jpg'], nargs='*')
 	args = parser.parse_args(args)
 
-	main()
+	if len(args.images) != 2:
+		parser.error('Specifiy exactly two images!')
+
+	main(args.images)
 
 if __name__ == '__main__':
 	main_cli()
