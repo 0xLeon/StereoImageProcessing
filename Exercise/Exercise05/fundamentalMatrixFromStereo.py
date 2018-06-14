04e961812733e547c7a48f068a1ee3997e672651
Stefan Hahn <Stefan_Hahn1@gmx.net>
2018-06-14 23:05:08 +0200

    Added loading / saving of maching resuts

--- Exercise/Exercise05/fundamentalMatrixFromStereo.py
+++ Exercise/Exercise05/fundamentalMatrixFromStereo.py
@@ -1,7 +1,86 @@
 import argparse
+import os
+import pickle
 
 import cv2
 import numpy as np
+
+#region Disk Saving
+
+#region Data Conversion
+def keypointToDict(keypoint):
+	return {
+		'angle': keypoint.angle,
+		'class_id': keypoint.class_id,
+		'octave': keypoint.octave,
+		'pt': keypoint.pt,
+		'response': keypoint.response,
+		'size': keypoint.size,
+	}
+
+def dictToKeypoint(keypoint):
+	return cv2.KeyPoint(
+		keypoint['pt'][0],
+		keypoint['pt'][1],
+		keypoint['size'],
+		keypoint['angle'],
+		keypoint['response'],
+		keypoint['octave'],
+		keypoint['class_id'],
+	)
+
+def matchToDict(match):
+	return {
+		'queryIdx': match.queryIdx,
+		'trainIdx': match.trainIdx,
+		'imgIdx': match.imgIdx,
+		'distance': match.distance,
+	}
+
+def dictToMatch(match):
+	return cv2.DMatch(
+		match['queryIdx'],
+		match['trainIdx'],
+		match['imgIdx'],
+		match['distance'],
+	)
+
+#endregion
+
+def saveStereoMatchingResult(kpA, desA, kpB, desB, matches, matchesMask, file='stereoMatch.pkl'):
+	kpA = list(map(keypointToDict, kpA))
+	kpB = list(map(keypointToDict, kpB))
+	matches = [(matchToDict(match[0]), matchToDict(match[1])) for match in matches]
+
+	data = dict(
+		kpA=kpA,
+		desA=desA,
+		kpB=kpB,
+		desB=desB,
+		matches=matches,
+		matchesMask=matchesMask,
+	)
+
+	if isinstance(file, str):
+		with open(file, 'wb') as f:
+			pickle.dump(data, f)
+	else:
+		pickle.dump(data, file)
+
+def loadStereoMatchingResult(file='stereoMatch.pkl'):
+	if isinstance(file, str):
+		with open(file, 'rb') as f:
+			data = pickle.load(f)
+	else:
+		data = pickle.load(file)
+
+	kpA = list(map(dictToKeypoint, data['kpA']))
+	kpB = list(map(dictToKeypoint, data['kpB']))
+	matches = [(dictToMatch(match[0]), dictToMatch(match[1])) for match in data['matches']]
+
+	return kpA, data['desA'], kpB, data['desB'], matches, data['matchesMask']
+
+#endregion
 
 def matchImages(imgA, imgB, nFeatures=50000, qualityThreshold=0.8):
 	orb = cv2.ORB_create(nFeatures)
@@ -22,24 +101,30 @@ def matchImages(imgA, imgB, nFeatures=50000, qualityThreshold=0.8):
 
 	return kpA, desA, kpB, desB, matches, matchesMask
 
-def main(images=None):
+def main(images=None, readMatch='', output='./'):
 	if images is None:
 		images = ['a.jpg', 'b.jpg']
 
 	imgA = cv2.imread(images[0])
 	imgB = cv2.imread(images[1])
 
-	kpA, desA, kpB, desB, matches, matchesMask = matchImages(imgA, imgB, qualityThreshold=0.65)
+	if readMatch:
+		kpA, desA, kpB, desB, matches, matchesMask = loadStereoMatchingResult(readMatch)
+	else:
+		kpA, desA, kpB, desB, matches, matchesMask = matchImages(imgA, imgB, qualityThreshold=0.65)
+		saveStereoMatchingResult(kpA, desA, kpB, desB, matches, matchesMask, os.path.join(output, 'stereoMatch.pkl'))
 
 def main_cli(args=None):
 	parser = argparse.ArgumentParser()
+	parser.add_argument('--readmatch', default='')
+	parser.add_argument('--output', default='./')
 	parser.add_argument('images', default=['a.jpg', 'b.jpg'], nargs='*')
 	args = parser.parse_args(args)
 
 	if len(args.images) != 2:
 		parser.error('Specifiy exactly two images!')
 
-	main(args.images)
+	main(args.images, args.readmatch, args.output)
 
 if __name__ == '__main__':
 	main_cli()
