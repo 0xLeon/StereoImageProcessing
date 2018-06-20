c3be113331d67139371c5b889e266b1f69a39fb9
Stefan Hahn <Stefan_Hahn1@gmx.net>
2018-06-20 18:44:47 +0200

    Separate data output to folders by laser status

--- Lab/DataAcquisition/depth-2.5m-640x480-6-NoLaser.bat
+++ Lab/DataAcquisition/depth-2.5m-640x480-6-NoLaser.bat
@@ -1,2 +1,2 @@
 @echo off
-py GetDepthFrames.py --nimages=10 --resolution=640,480 --fps=6 --output=data/RealSense-D415-Plane-2.5m-640x480-6-NoLaser/
+py GetDepthFrames.py --nimages=10 --resolution=640,480 --fps=6 --output=data/NoLaser/RealSense-D415-Plane-2.5m-640x480-6-NoLaser/
