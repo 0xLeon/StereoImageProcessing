729ffb099c13875a906139fa0ea1fd457c068dc8
Stefan Hahn <Stefan_Hahn1@gmx.net>
2018-06-20 18:35:51 +0200

    Apply abs and transpose directly at interval generation

--- Lab/PlotFitErrors.py
+++ Lab/PlotFitErrors.py
@@ -9,9 +9,9 @@ if __name__ == '__main__':
 		data = pickle.load(f)
 
 	data = data['1280x720']
-	intervals = np.array([st.t.interval(0.95, len(data[i])-1, loc=0, scale=st.sem(data[i])) for i in data])
+	intervals = np.abs(np.array([st.t.interval(0.95, len(data[i])-1, loc=0, scale=st.sem(data[i])) for i in data])).T
 	means = [np.mean(data[i]) for i in data]
 
-	plt.errorbar(data.keys(), means, np.abs(intervals).T, fmt='o-', ecolor='k', elinewidth=0.7, barsabove=True, capsize=2.5)
+	plt.errorbar(data.keys(), means, intervals, fmt='o-', ecolor='k', elinewidth=0.7, barsabove=True, capsize=2.5)
 	plt.grid(True)
 	plt.show()
