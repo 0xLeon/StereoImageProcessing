3835fa54f05ce7ea5b80d285beae69a7a8eaf339
Stefan Hahn <Stefan_Hahn1@gmx.net>
2018-06-20 17:34:20 +0200

    Re-arranged code for simpler addition of subplots

--- Lab/PlotGarbageFiltering.py
+++ Lab/PlotGarbageFiltering.py
@@ -11,24 +11,28 @@ if __name__ == '__main__':
 		raise ValueError('Invalid parameters')
 
 	vertices = PLYObject.PLYObject(sys.argv[1]).getVertices().T
-	data0 = np.unique(np.sort(vertices[:, 2]))
-	data1 = np.diff(data0)
-
-	fVertices = FilterPointCloud.filterGarbage(vertices)
-	data2 = np.unique(np.sort(fVertices[:, 2]))
-	data3 = np.diff(data2)
 
 	fig, ax = plt.subplots(1, 2, sharex=True, sharey=False)
 	fig.suptitle('Garbage Point Filtering', fontsize=16)
+
+	for axis in ax:
+		axis.grid(True)
+		axis.set_ylabel('Z values')
+
+	data0 = np.unique(np.sort(vertices[:, 2]))
+	data1 = np.diff(data0)
+
 	ax[0].set_title('Without Garbage Filtering')
-	ax[0].set_ylabel('Z values')
-	ax[0].grid(True)
 	ax[0].plot(data0)
 	ax[0].plot(data1, '--')
+
+	fVertices = FilterPointCloud.filterGarbage(vertices)
+	data0 = np.unique(np.sort(fVertices[:, 2]))
+	data1 = np.diff(data0)
+
 	ax[1].set_title('With Garbage Filtering')
-	ax[1].set_ylabel('Z values')
-	ax[1].grid(True)
-	ax[1].plot(data2)
-	ax[1].plot(data3, '--')
+	ax[1].plot(data0)
+	ax[1].plot(data1, '--')
+
 	plt.get_current_fig_manager().window.state('zoomed')
 	plt.show()
