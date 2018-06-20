22f97a1fd8e981212b43312f98d889055bf20655
Stefan Hahn <Stefan_Hahn1@gmx.net>
2018-06-20 17:44:00 +0200

    Plot both v1 and v2 of garbage filtering algorithm

--- Lab/PlotGarbageFiltering.py
+++ Lab/PlotGarbageFiltering.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
 
 	vertices = PLYObject.PLYObject(sys.argv[1]).getVertices().T
 
-	fig, ax = plt.subplots(1, 2, sharex=True, sharey=False)
+	fig, ax = plt.subplots(1, 3, sharex=True, sharey=True)
 	fig.suptitle('Garbage Point Filtering', fontsize=16)
 
 	for axis in ax:
@@ -26,13 +26,21 @@ if __name__ == '__main__':
 	ax[0].plot(data0)
 	ax[0].plot(data1, '--')
 
+	fVertices = FilterPointCloud.filterGarbageOld1(vertices)
+	data0 = np.unique(np.sort(fVertices[:, 2]))
+	data1 = np.diff(data0)
+
+	ax[1].set_title('With Garbage Filtering v1')
+	ax[1].plot(data0)
+	ax[1].plot(data1, '--')
+
 	fVertices = FilterPointCloud.filterGarbage(vertices)
 	data0 = np.unique(np.sort(fVertices[:, 2]))
 	data1 = np.diff(data0)
 
-	ax[1].set_title('With Garbage Filtering')
-	ax[1].plot(data0)
-	ax[1].plot(data1, '--')
+	ax[2].set_title('With Garbage Filtering v2')
+	ax[2].plot(data0)
+	ax[2].plot(data1, '--')
 
 	plt.get_current_fig_manager().window.state('zoomed')
 	plt.show()
