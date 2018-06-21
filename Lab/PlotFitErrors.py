78e82814d236773c6f32c2c5b9fca6da8ba5ff00
Stefan Hahn <Stefan_Hahn1@gmx.net>
2018-06-21 10:00:36 +0200

    Plot set of curves over all available resolutions

--- Lab/PlotFitErrors.py
+++ Lab/PlotFitErrors.py
@@ -8,10 +8,14 @@ if __name__ == '__main__':
 	with open('./RealSense-D415-Data-01/Laser/PlaneFitErrorData.pkl', 'rb') as f:
 		data = pickle.load(f)
 
-	data = data['1280x720']
-	intervals = np.abs(np.array([st.t.interval(0.95, len(data[i])-1, loc=0, scale=st.sem(data[i])) for i in data])).T
-	means = [np.mean(data[i]) for i in data]
+	cdata = dict(zip(data.keys(), ['b', 'g', 'r']))
 
-	plt.errorbar(data.keys(), means, intervals, fmt='o-', ecolor='k', elinewidth=0.7, barsabove=True, capsize=2.5)
+	for res in data:
+		intervals = np.abs(np.array([st.t.interval(0.95, len(data[res][i])-1, loc=0, scale=st.sem(data[res][i])) for i in data[res]])).T
+		means = [np.mean(data[res][i]) for i in data[res]]
+
+		plt.errorbar(data[res].keys(), means, intervals, fmt='{:s}o-'.format(cdata[res]), ms=5, ecolor='r', elinewidth=0.8, barsabove=True, capsize=3, alpha=0.4, label=res)
+
 	plt.grid(True)
+	plt.legend()
 	plt.show()
