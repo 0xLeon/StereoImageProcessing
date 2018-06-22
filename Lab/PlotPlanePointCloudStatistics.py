9a130713aca27ef092c6cb9f165a50b143283340
Stefan Hahn <Stefan_Hahn1@gmx.net>
2018-06-22 22:21:47 +0200

    Move __main__ part to main() function

--- Lab/PlotPlanePointCloudStatistics.py
+++ Lab/PlotPlanePointCloudStatistics.py
@@ -1,4 +1,6 @@
+import os
 import pickle
+import sys
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -10,19 +12,27 @@ def plotToSubplot(axis, data, curveSetKeys, curveSetKey):
 	intervals = np.abs(np.array([st.t.interval(0.95, len(data[i])-1, loc=0, scale=st.sem(data[i])) for i in data])).T
 	means = [np.mean(data[i]) for i in data]
 
-	axis.errorbar(data.keys(), means, intervals, fmt='{:s}o-'.format(cdata[curveSetKey]), ms=5, ecolor='r', elinewidth=0.8, barsabove=True, capsize=3, alpha=0.4, label=curveSetKey)
+	axis.errorbar(data.keys(), means, intervals, fmt='{:s}o-'.format(cdata[curveSetKey]), ms=5, elinewidth=0.8, barsabove=True, capsize=4, alpha=0.7, label=curveSetKey)
 	axis.grid(True)
 	axis.legend()
 
-if __name__ == '__main__':
-	with open('./RealSense-D415-Data-01-Near/Laser/PointCloudProcessingData.pkl', 'rb') as f:
+def main(dataFilePath):
+	with open(dataFilePath, 'rb') as f:
 		data = pickle.load(f)
 
-	fig, ax = plt.subplots(1, 2, sharex=True, sharey=False)
+	fig, ax = plt.subplots(1, 2, sharex=True, sharey=False, figsize=(15, 7.5))
 	resolutions = data.keys()
 
 	for res in data:
 		plotToSubplot(ax[0], data[res]['planeFitError'], resolutions, res)
 		plotToSubplot(ax[1], data[res]['pointDensity'], resolutions, res)
 
-	plt.show()
+	# plt.show()
+	graphFilePath = os.path.join(os.path.dirname(dataFilePath), 'PointCloudProcessingData.png')
+	fig.savefig(graphFilePath, dpi=300, bbox_inches='tight')
+
+if __name__ == '__main__':
+	if len(sys.argv) < 2:
+		sys.argv.append('./RealSense-D415-Data-01-Near/Laser/PointCloudProcessingData.pkl')
+
+	main(sys.argv[1])
