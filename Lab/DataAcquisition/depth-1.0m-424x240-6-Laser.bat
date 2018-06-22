a293f726a34c9d752ab2763e74da1bf23cedf297
Stefan Hahn <Stefan_Hahn1@gmx.net>
2018-06-22 17:07:14 +0200

    Added scripts for lowest supported depth resolution

---
+++ Lab/DataAcquisition/depth-1.0m-424x240-6-Laser.bat
@@ -0,0 +1,2 @@
+@echo off
+py GetDepthFrames.py --nimages=10 --resolution=424,240 --fps=6 --laser --output=data/Laser/RealSense-D415-Plane-1.0m-424x240-6-Laser/
---
+++ Lab/DataAcquisition/depth-7.5m-424x240-6-Laser.bat
@@ -0,0 +1,2 @@
+@echo off
+py GetDepthFrames.py --nimages=10 --resolution=424,240 --fps=6 --laser --output=data/Laser/RealSense-D415-Plane-7.5m-424x240-6-Laser/
