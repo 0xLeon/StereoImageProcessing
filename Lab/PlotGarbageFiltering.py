e03f8594bd3caa5471c1db9ef7170e83a5550009
Stefan Hahn <Stefan_Hahn1@gmx.net>
2018-06-27 14:18:16 +0200

    Plot all garbage filtering versions in a 2x2 plot

--- Lab/PlotGarbageFiltering.py
+++ Lab/PlotGarbageFiltering.py
@@ -12,35 +12,43 @@ if __name__ == '__main__':
 
 	vertices = PLYObject.PLYObject(sys.argv[1]).getVertices().T
 
-	fig, ax = plt.subplots(1, 3, sharex=True, sharey=True)
+	fig, ax = plt.subplots(2, 2, sharex=True, sharey=True)
 	fig.suptitle('Garbage Point Filtering', fontsize=16)
 
-	for axis in ax:
+	for axis in ax.ravel():
 		axis.grid(True)
 		axis.set_ylabel('Z values')
 
 	data0 = np.unique(np.sort(vertices[:, 2]))
 	data1 = np.diff(data0)
 
-	ax[0].set_title('Without Garbage Filtering')
-	ax[0].plot(data0)
-	ax[0].plot(data1, '--')
+	ax[0, 0].set_title('Without Garbage Filtering')
+	ax[0, 0].plot(data0)
+	ax[0, 0].plot(data1, '--')
 
 	fVertices = FilterPointCloud.filterGarbageOld1(vertices)
 	data0 = np.unique(np.sort(fVertices[:, 2]))
 	data1 = np.diff(data0)
 
-	ax[1].set_title('With Garbage Filtering v1')
-	ax[1].plot(data0)
-	ax[1].plot(data1, '--')
+	ax[0, 1].set_title('With Garbage Filtering v1')
+	ax[0, 1].plot(data0)
+	ax[0, 1].plot(data1, '--')
+
+	fVertices = FilterPointCloud.filterGarbageOld2(vertices)
+	data0 = np.unique(np.sort(fVertices[:, 2]))
+	data1 = np.diff(data0)
+
+	ax[1, 0].set_title('With Garbage Filtering v2')
+	ax[1, 0].plot(data0)
+	ax[1, 0].plot(data1, '--')
 
 	fVertices = FilterPointCloud.filterGarbage(vertices)
 	data0 = np.unique(np.sort(fVertices[:, 2]))
 	data1 = np.diff(data0)
 
-	ax[2].set_title('With Garbage Filtering v2')
-	ax[2].plot(data0)
-	ax[2].plot(data1, '--')
+	ax[1, 1].set_title('With Garbage Filtering v3')
+	ax[1, 1].plot(data0)
+	ax[1, 1].plot(data1, '--')
 
 	plt.get_current_fig_manager().window.state('zoomed')
 	plt.show()
