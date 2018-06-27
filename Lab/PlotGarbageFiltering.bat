c9a3802935dc1b73fc131ead99bf92c02dacf87c
Stefan Hahn <Stefan_Hahn1@gmx.net>
2018-06-27 14:18:42 +0200

    Adjust data path

--- Lab/PlotGarbageFiltering.bat
+++ Lab/PlotGarbageFiltering.bat
@@ -1,2 +1,2 @@
 @echo off
-py PlotGarbageFiltering.py .\RealSense-D415-Data-01\Laser\RealSense-D415-Plane-0.5m-640x480-6-Laser\3.pointcloud.ply
+py PlotGarbageFiltering.py .\RealSense-D415-Data-01-Near\Laser\RealSense-D415-Plane-0.5m-640x480-6-Laser\3.pointcloud.ply
