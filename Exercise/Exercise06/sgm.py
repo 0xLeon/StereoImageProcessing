d47173f667fb218b75ffb05a60b42008ec3d1614
Stefan <Stefan_Hahn1@gmx.net>
2018-07-04 12:23:04 +0200

    Calculate sum over correct axis

--- Exercise/Exercise06/sgm.py
+++ Exercise/Exercise06/sgm.py
@@ -81,7 +81,7 @@ def main(imgL, imgR, disparityRange=(0, 20), directions=8):
 			p += direction[0]
 			p = p[(p[:, 0] > -1) & (p[:, 0] < imgL.shape[1]) & (p[:, 1] > -2) & (p[:, 1] < imgL.shape[0])]
 
-	S = Lr.sum(axis=1)
+	S = Lr.sum(axis=0)
 	dispImage = S.min(axis=2)
 
 def main_cli():
