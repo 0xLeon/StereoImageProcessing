bbe0ac931da708dcc60860fde5167c9852b09317
Stefan <Stefan_Hahn1@gmx.net>
2018-07-13 15:26:52 +0200

    Added time measurement context manager

--- Exercise/Exercise07/colorClassification.py
+++ Exercise/Exercise07/colorClassification.py
@@ -3,12 +3,37 @@ import glob
 import itertools
 import os
 import pathlib
+import time
 
 import cv2
 import numpy as np
 import sklearn
 import sklearn.model_selection
 import sklearn.preprocessing
+
+class TimeMeasurement(object):
+	def __init__(self, operation, printStd=True):
+		self._t0 = 0
+		self._t1 = 0
+
+		self._operation = operation
+		self._printStd = printStd
+
+	@property
+	def delta(self):
+		return self._t1 - self._t0
+
+	def __enter__(self):
+		self._t1 = 0
+		self._t0 = time.perf_counter()
+
+		return self
+
+	def __exit__(self, *args):
+		self._t1 = time.perf_counter()
+
+		if self._printStd:
+			print('Operation \'{!s}\' took {:.4f} s'.format(self._operation, self.delta))
 
 def findImages(directory='.'):
 	rawLabels = list(filter(os.path.isdir, os.listdir(directory)))
@@ -25,7 +50,8 @@ def findImages(directory='.'):
 	return imageSet, imageSetLabels, labelEncoder
 
 def main():
-	images, labels, encoder = findImages()
+	with TimeMeasurement('Find Images'):
+		images, labels, encoder = findImages()
 
 	return
 
